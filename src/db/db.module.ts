3592a1a31e9ae6443a0361ec57ced443fad7070d
koohpeyma <rezakoohpeyma2@gmail.com>
2026-06-18 00:08:37 +0330

    feat: setup drizzle

---
+++ src/db/db.module.ts
@@ -0,0 +1,14 @@
+import { Global, Module } from '@nestjs/common';
+import { db } from './index'; 
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'DB',
+      useValue: db,
+    },
+  ],
+  exports: ['DB'],
+})
+export class DbModule {}
