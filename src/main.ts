4b7e6404313ae2cc803099f19f63af688242cab1
koohpeyma <rezakoohpeyma2@gmail.com>
2026-06-17 20:11:39 +0330

    feat : setup security

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-
+import helmet from "helmet";
+import compression	from "compression";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(
@@ -11,6 +12,8 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }),
 );
+app.use(helmet());
+app.use(compression());
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
